9bc395ae74961aed862b7a6d9a25a22ee12e845c
j-chor <jakub.rrk@outlook.com>
2023-05-25 09:31:25 +0200

    install fs, add styles

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ console.log("Hi");
 
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.set("view engine", "ejs");
+
+const style = fs.readFileSync('views/styles.css');
+app.get('/styles.css', (req, res) => {
+    res.set('Content-Type', 'text/css');
+    res.send(style);
+});
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
 
 app.listen(3000);
 
